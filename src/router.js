ed6ceaf917f56775652b17c07ee99deaf09c4540
unknown <ljh001011@gmail.com>
2024-07-22 16:50:20 +0900

    2nd commit

--- src/router.js
+++ src/router.js
@@ -3,10 +3,12 @@ const router = express.Router(); // express의 Router 인스턴스 생성
 
 const multer = require('multer');
 const upload = multer({ dest: 'storage/' });
+const authenticateToken = require('./middleware/authenticate');
 
 const webController = require('./web/controller');
 const apiFeedController = require('./api/feed/controller');
 const apiUserController = require('./api/user/controller');
+const fileController = require('./api/file/controller');
 
 const { logRequestTime } = require('./middleware/log');
 
@@ -15,14 +17,18 @@ router.get('/page/:route', webController.page);
 
 router.use(logRequestTime);
 
-router.post('/file', upload.single('file'), (req, res) => {
-    console.log(req.file)
-    res.json(req.file);
-});
+router.post('/file', upload.single('file'), fileController.upload);
+router.get('/file/:id', fileController.download);
 
-router.get('/api/phone', apiUserController.phone);
-router.put('/api/phone', apiUserController.phoneVerify);
-router.post('/api/register', apiUserController.register);
+// 사용자 관련 라우트
+router.post('/auth/phone', apiUserController.phone);
+router.put('/auth/phone', apiUserController.phoneVerify);
+router.post('/auth/register', apiUserController.register);
+router.post('/auth/login', apiUserController.login);
+
+// 피드 관련 라우트, 모든 요청에 인증 필요
+router.use(authenticateToken); // 이후 모든 라우트에 인증 적용
+
 router.get('/api/user/my', apiUserController.show);
 router.post('/api/user/my', apiUserController.update);
 
